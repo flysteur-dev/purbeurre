b1f5541f8b8ce6fb4feb76a6448d6824fa064eb2
OCTAU Alexandre <alexandre@octau.me>
2019-03-29 01:33:27 +0100

    Updates

---
+++ precache-manifest.1da42335d550626e5484f36f23ef2cae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "60dd33af31c1b62528b6",
+    "url": "/purbeurre/static/js/runtime~main.4a592724.js"
+  },
+  {
+    "revision": "7d0a48210d25c4dda1af",
+    "url": "/purbeurre/static/js/main.be20ed6f.chunk.js"
+  },
+  {
+    "revision": "37aec3aaabd0905542c4",
+    "url": "/purbeurre/static/js/2.8ed3e6c7.chunk.js"
+  },
+  {
+    "revision": "7d0a48210d25c4dda1af",
+    "url": "/purbeurre/static/css/main.11129608.chunk.css"
+  },
+  {
+    "revision": "37aec3aaabd0905542c4",
+    "url": "/purbeurre/static/css/2.159b4675.chunk.css"
+  },
+  {
+    "revision": "8546a315bbdafad7562a72c06281fe77",
+    "url": "/purbeurre/index.html"
+  }
+];
